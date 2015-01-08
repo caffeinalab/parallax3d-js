6d011180f5374bb02d4664c6e8377402fa6ca071
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2015-01-08 14:32:03 +0100

    Included prefix

--- parallax3d.js
+++ parallax3d.js
@@ -1,3 +1,19 @@
+var prefix = (function() {
+	var styles = window.getComputedStyle(document.documentElement, ''),
+	pre = (Array.prototype.slice
+	.call(styles)
+	.join('')
+	.match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+	)[1],
+	dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
+	return {
+		dom: dom,
+		lowercase: pre,
+		css: '-' + pre + '-',
+		js: pre[0].toUpperCase() + pre.substr(1)
+	};
+})();
+
 var Parallax3D = (function() {
 	var self = {};
 	self.elements = [];
